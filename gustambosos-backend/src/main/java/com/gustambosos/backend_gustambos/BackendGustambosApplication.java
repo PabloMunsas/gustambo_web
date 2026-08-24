62fe38e0d3a610405a480a7fe9961f28279fe785
Pablo <pabloms.prof26@gmail.com>
2026-08-25 00:53:48 +0200

    Creación del proyecto y merge a la develop

---
+++ gustambosos-backend/src/main/java/com/gustambosos/backend_gustambos/BackendGustambosApplication.java
@@ -0,0 +1,13 @@
+package com.gustambosos.backend_gustambos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGustambosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGustambosApplication.class, args);
+	}
+
+}
---
+++ gustambosos-backend/src/test/java/com/gustambosos/backend_gustambos/BackendGustambosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustambosos.backend_gustambos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGustambosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
